b7c4d005f7c3fb150e63392ca15cf25a1cefe291
Sang10704 <Sanghuynh10072000@gmail.com>
2025-11-05 09:36:20 +0700

    .

--- eProject/database/migrations/2014_10_12_000000_create_users_table.php
+++ eProject/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('specialization'); // chuyên ngành
+        $table->integer('experience_years')->nullable();
+        $table->string('location'); // tỉnh / thành phố
+        $table->text('bio')->nullable();
+        $table->string('license_number')->nullable();
+        $table->boolean('verified')->default(false);
+        $table->timestamps();
         });
     }
 
